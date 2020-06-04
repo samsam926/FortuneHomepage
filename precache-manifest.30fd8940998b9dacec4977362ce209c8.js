6949e3fbf55b214ef5d0c92e18468c4f33d9479a
Haitham Magdy <44612847+samsam926@users.noreply.github.com>
2020-06-04 20:49:08 +0200

    FortuneFirst

---
+++ precache-manifest.30fd8940998b9dacec4977362ce209c8.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c036111ef7731b8920c896148017710e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc99e213c986b6eb56f8",
+    "url": "/static/css/2.653cead1.chunk.css"
+  },
+  {
+    "revision": "f3abf3b271703926281f",
+    "url": "/static/css/main.28b57445.chunk.css"
+  },
+  {
+    "revision": "dc99e213c986b6eb56f8",
+    "url": "/static/js/2.bb185e4c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.bb185e4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3abf3b271703926281f",
+    "url": "/static/js/main.13b2b682.chunk.js"
+  },
+  {
+    "revision": "df98bc9177f01ea4407f",
+    "url": "/static/js/runtime-main.3176ff0b.js"
+  },
+  {
+    "revision": "dc6c0221d7eb0d43fab2cc7617335af1",
+    "url": "/static/media/360Cam.dc6c0221.jpg"
+  },
+  {
+    "revision": "f5cd4384c3b49c00b16c190b318bb096",
+    "url": "/static/media/brand-image-1.f5cd4384.png"
+  },
+  {
+    "revision": "89f1737968d816badad4f32400485c68",
+    "url": "/static/media/brand-image-2.89f17379.png"
+  },
+  {
+    "revision": "8c234b9e19c1b1b0f7358a12f081f69c",
+    "url": "/static/media/brand-image-3.8c234b9e.png"
+  },
+  {
+    "revision": "ea9b1c5bb436d779343eedbb141107cb",
+    "url": "/static/media/brand-logo-1.ea9b1c5b.png"
+  },
+  {
+    "revision": "ee41804711a3563b77d15bd6b2dedace",
+    "url": "/static/media/brand-logo-3.ee418047.png"
+  },
+  {
+    "revision": "0ac3c6abf6ee826a43a02aba227eb7a5",
+    "url": "/static/media/part-2.0ac3c6ab.png"
+  },
+  {
+    "revision": "411adfd955ab2678beaf66fb358c7fa3",
+    "url": "/static/media/part-3.411adfd9.png"
+  },
+  {
+    "revision": "071bbf2dfc6e9650b24d6115bcac6baa",
+    "url": "/static/media/part-4.071bbf2d.png"
+  },
+  {
+    "revision": "c44611c39811fcccf8d0bff33b8a9ee6",
+    "url": "/static/media/part-5.c44611c3.png"
+  },
+  {
+    "revision": "57e29317ad6d0f14271799450c924ab2",
+    "url": "/static/media/part-6.57e29317.png"
+  },
+  {
+    "revision": "9b75f5f59fd2255badf46741164b11cf",
+    "url": "/static/media/product-1.9b75f5f5.jpg"
+  },
+  {
+    "revision": "912477f1db079a555433cab9736141b7",
+    "url": "/static/media/product-2.912477f1.jpg"
+  },
+  {
+    "revision": "9f4337b0ee9a52bc78e0c83275f21e97",
+    "url": "/static/media/product-3.9f4337b0.jpg"
+  },
+  {
+    "revision": "090e9a6780e12e98c9d886909497f3b9",
+    "url": "/static/media/product-4.090e9a67.jpg"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/static/media/revicons.2feb69cc.eot"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "9bb8ac5bbf0d2173dd34f97f68c56cc8",
+    "url": "/static/media/slider-1.9bb8ac5b.png"
+  },
+  {
+    "revision": "434659a320152ea5b669427d4ffa3247",
+    "url": "/static/media/slider-2.434659a3.jpg"
+  },
+  {
+    "revision": "d82a0e549c228dc5fae4dd4c7f97cecc",
+    "url": "/static/media/title-arrow.d82a0e54.svg"
+  }
+]);
